b9c2cab7f557768e9c7548282e2ab0f9a4f8b6da
Sebastian (Ristarian) <50881784+Ristarian@users.noreply.github.com>
2020-02-17 18:55:43 +0100

    First commit with code

---
+++ script.js
@@ -0,0 +1,24 @@
+// wezwanie funkcji
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+// funkcja wzywana
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+// nadawanie rotacji wskazówkom
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
